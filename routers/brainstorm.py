b0a910a4e711428fbb477a1f422c3e8fb361e53d
Jack Mu <jackmu@umich.edu>
2024-11-17 03:43:59 -0800

    small snippet to accomodate trailing / in url :

--- routers/brainstorm.py
+++ routers/brainstorm.py
@@ -6,5 +6,7 @@ from services.crawler import crawlSite
 router = APIRouter()
 @router.post("/brainstorm/")
 async def brainstorm(urlObject: UrlObject):
+    if urlObject.url[-1] == "/":
+        urlObject.url = urlObject.url[0:-1]
     texts = crawlSite(urlObject.url)
     return promptLlm(texts)
